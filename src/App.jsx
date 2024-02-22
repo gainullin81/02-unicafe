b40868cfb31e5ad8a6a1d3d4b5c3d0df4b85375c
alex Gainullin <againullin8@gmail.com>
2024-02-22 10:26:37 +0200

    1.7: unicafe step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-//1.6: unicafe step1
+//1.7: unicafe step2
 import { useState } from 'react'
 
 const App = () => {
@@ -6,7 +6,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const all = bad+good+neutral
+  const average = (good - bad)/all
+  const positive = (good / all)*100
 
   return (
      <div>
@@ -19,6 +21,9 @@ const App = () => {
     <p>good: {good}</p>
     <p>neutral: {neutral}</p>
     <p>bad: {bad}</p>
+    <p>all: {all}</p>
+    <p>average: {average}</p>
+    <p>positive: {positive} %</p>
   </div>
   ) }
   
